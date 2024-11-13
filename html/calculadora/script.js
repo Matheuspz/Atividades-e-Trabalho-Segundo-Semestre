edd4927b385f609e0c618445f2cc345e2652a0ac
Matheus Pesch Zenere <93321651+Matheuspz@users.noreply.github.com>
2024-11-13 20:11:18 -0300

    Add files via upload

--- html/calculadora/script.js
+++ html/calculadora/script.js
@@ -1,15 +1,79 @@
-/*function Somar() {
-    let x = document.getElementById("num1").value;
-    let y = document.getElementById("num2").value;
-    console.log(typeof(x))
-    console.log(typeof(y))
-    const newDiv = document.createElement("div");
-    const newP = document.createElement("p");
-    newDiv.append(newP)
-    document.body.append(newDiv)
-    
-}*/
+let valorInicial = "";
+let operador = "";
+let resultado = "";
+
 function atualizaDisplay(valor) {
-    const visor = document.getElementById("visor")
-    visor.value = visor.value + valor
+    const visor = document.getElementById("visor");
+    visor.value = visor.value + valor;
+}
+function limparVisor() {
+    const visor = document.getElementById("visor");
+    visor.value = "";
+}
+function deletar() {
+    const visor = document.getElementById("visor");
+    visor.value = visor.value.slice(0, -1);
+}
+
+
+
+function somar() {
+    const visor = document.getElementById("visor");
+    valorInicial = visor.value;
+    operador = "+";
+    visor.value = "";
+}
+function subtrair() {
+    const visor = document.getElementById("visor");
+    valorInicial = visor.value;
+    operador = "-";
+    visor.value = "";
+}
+function multiplicar() {
+    const visor = document.getElementById("visor");
+    valorInicial = visor.value;
+    operador = "*";
+    visor.value = "";
+}
+function dividir() {
+    const visor = document.getElementById("visor");
+    valorInicial = visor.value;
+    operador = "/";
+    visor.value = "";
+}
+function porcentagem() {
+    const visor = document.getElementById("visor");
+    valorInicial = visor.value;
+    visor.value = parseFloat(valorInicial) / 100
+}
+function alternarSinal() {
+    const visor = document.getElementById("visor");
+    valorInvert = parseFloat(visor.value);
+    visor.value = (-valorInvert).toString();
+}
+
+
+
+function calcular() {
+    const visor = document.getElementById("visor");
+    let valorFinal = visor.value;
+    switch (operador) {
+        case "+":
+            resultado = parseFloat(valorInicial) + parseFloat(valorFinal);
+            break;
+        case "-":
+            resultado = parseFloat(valorInicial) - parseFloat(valorFinal);
+            break;
+        case "*":
+            resultado = parseFloat(valorInicial) * parseFloat(valorFinal);
+            break;
+        case "/":
+            resultado = parseFloat(valorInicial) / parseFloat(valorFinal);
+            break;
+        default:
+            return 0;
+    }
+    visor.value = resultado;
+    valorInicial = "";
+    operador = "";
 }
